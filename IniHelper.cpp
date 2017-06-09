546100bfdd4993b81d58925eed31cd38c40c4449
mingkuang <mingkuang@live.com>
2017-06-09 11:00:12 +0800

    Ini改进Windows XP支持

--- IniHelper.cpp
+++ IniHelper.cpp
@@ -4,24 +4,23 @@
 
 BOOL IniReadString(LPCWSTR FilePath, LPCWSTR KeyPath, LPCWSTR ValueName, CString& Str)
 {
-	//Str.GetAllocLength();
-
 	int cchData = Str.GetAllocLength()>100 ? Str.GetAllocLength() : 100;
 
-	while (cchData = GetPrivateProfileStringW(KeyPath, ValueName, NULL, Str.GetBuffer(cchData), cchData, FilePath))
+	while (auto chNewData = GetPrivateProfileStringW(KeyPath, ValueName, NULL, Str.GetBuffer(cchData), cchData, FilePath))
 	{
-		if (GetLastError() == ERROR_MORE_DATA)
+		if (cchData - chNewData <= 2)
 		{
-			cchData = Str.GetAllocLength()*Str.GetAllocLength();
+			//持续加大缓冲区
+			cchData *= 2;
 		}
 		else
 		{
-			Str.ReleaseBuffer(cchData);
-			return 1;
+			Str.ReleaseBuffer(chNewData);
+			return TRUE;
 		}
 	}
 
-	return 0;
+	return FALSE;
 }
 
 BOOL IniReadBinaryData(LPCWSTR FilePath, LPCWSTR KeyPath, LPCWSTR ValueName, CStringA& Data)
@@ -57,19 +56,17 @@ BOOL IniDeleteString(LPCWSTR FilePath, LPCWSTR KeyPath, LPCWSTR ValueName)
 
 BOOL IniGetSectionNames(LPCWSTR FilePath, CString& Names)
 {
-	//Str.GetAllocLength();
-
 	int cchData = Names.GetAllocLength()>100 ? Names.GetAllocLength() : 100;
 
-	while (cchData = GetPrivateProfileSectionNames(Names.GetBuffer(cchData), cchData, FilePath))
+	while (auto chNewData = GetPrivateProfileSectionNamesW(Names.GetBuffer(cchData), cchData, FilePath))
 	{
-		if (GetLastError() == ERROR_MORE_DATA)
+		if (cchData - chNewData == 2)
 		{
-			cchData = Names.GetAllocLength()*Names.GetAllocLength();
+			cchData *= 2;
 		}
 		else
 		{
-			Names.ReleaseBuffer(cchData);
+			Names.ReleaseBuffer(chNewData);
 			return TRUE;
 		}
 	}
@@ -80,16 +77,15 @@ BOOL IniGetSectionNames(LPCWSTR FilePath, CString& Names)
 
 BOOL IniGetValues(LPCWSTR FilePath, LPCWSTR Path, CString& Values)
 {
-	//Str.GetAllocLength();
 	Values.GetBuffer(1024);
 
 	int cchData = Values.GetAllocLength();
 
-	while (cchData = GetPrivateProfileSection(Path, Values.GetBuffer(cchData), cchData, FilePath))
+	while (auto chNewData = GetPrivateProfileSectionW(Path, Values.GetBuffer(cchData), cchData, FilePath))
 	{
-		if (cchData == Values.GetAllocLength() - 2)
+		if (cchData - chNewData == 2)
 		{
-			cchData = Values.GetAllocLength()*Values.GetAllocLength();
+			cchData *= 2;
 		}
 		else
 		{
